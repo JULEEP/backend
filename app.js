ef6ef19d429a1e5f8483f061998b737890fa21c5
JULEEP <115367178+JULEEP@users.noreply.github.com>
2023-10-10 16:36:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
 
-// Dotenv is a zero-dependency module that loads environment variables from a .env file into process.env
+
 
 const app = express();
 app.use(bodyParser.json());
@@ -24,12 +24,10 @@ app.use(cors());
 // });
 
 const connectDB = require('./config/db');
-// Load Config
 dotenv.config({path: './config/config.env'})
 
 connectDB();
 
-// Routes
 app.use('/', require('./routes/index'));
 
 app.listen(3000);
